629fcfd16eb6263902b6aecb74586171cecb5ca5
sviluppatori3 <sviluppatori3@mais.services>
2025-05-21 00:02:38 +0200

    Style review

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -29,4 +29,11 @@ export class SideBarComponent implements OnInit {
   logout() {
     this.authService.logout();
   }
+
+  togggleTheme(){
+    if(document.body.getAttribute('data-theme') == 'light')
+      document.body.setAttribute('data-theme', 'dark');
+    else
+      document.body.setAttribute('data-theme', 'light');
+  }
 }
