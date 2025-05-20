a39a355ce5367443a3d0821a3f08c72d505cc1d8
sviluppatori3 <sviluppatori3@mais.services>
2025-05-20 15:27:39 +0200

    Microsoft auth v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,29 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { MsalGuard } from '@azure/msal-angular';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HomeComponent} from './home/home.component';
 
-const routes: Routes = [];
 
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: DashboardComponent,
+    canActivate: [
+      MsalGuard
+    ]
+  },
+  {
+    path: '**',
+    component: HomeComponent
+  }
+];
+
+// Create an NgModule that contains all the directives for the routes specified above
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    useHash: false
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
