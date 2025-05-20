e4a979aaa055ef3942860f54f8fd1e8acd06c8d3
sviluppatori3 <sviluppatori3@mais.services>
2025-05-20 15:37:51 +0200

    edit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,7 +7,7 @@ import {HomeComponent} from './home/home.component';
 
 const routes: Routes = [
   {
-    path: 'profile',
+    path: 'dashboard',
     component: DashboardComponent,
     canActivate: [
       MsalGuard
